1f6b708f84fd05c454c1f6ce05e0ed7a810a06e1
MarSam <marius.sam98@gmail.com>
2025-02-24 19:04:45 +0200

    Scrollbar fix

--- app/(main)/templates/page.tsx
+++ app/(main)/templates/page.tsx
@@ -10,7 +10,7 @@ export default async function Page() {
   const topics = await GetTopics();
 
   return (
-    <div className="h-full w-full overflow-y-scroll">
+    <div className="h-full w-full overflow-hidden">
       <div className="p-6">
         {templates.length === 0 ? (
           <p className="text-center text-muted-foreground text-xl font-semibold">
